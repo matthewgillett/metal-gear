cc6808f999d1868c572059f0a57eec6be274be43
Shehroze Khan <toberoo@gmail.com>
2016-08-09 10:49:45 -0400

    fixed smiley

--- src/main/webapp/src/actions/request_smiley.js
+++ src/main/webapp/src/actions/request_smiley.js
@@ -3,7 +3,7 @@ import emotion from './emotion'
 
 module.exports = function(parameter) {
 	return dispatch => {
-		var url = 'http://localhost:8080/twiter/sentiment/' + parameter;
+		var url = 'http://localhost:8080//twitter/sentiment/' + parameter;
 		console.log(url);
 		return fetch(url)
 		.then(function(response) {
